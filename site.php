08edce64f79fdaa5db3117eb1bb0f96dbab05beb
Isabelle Brandao <isabellebrandao@outlook.com>
2022-09-22 00:32:57 -0300

    Podutos Vs Categorias

--- site.php
+++ site.php
@@ -1,6 +1,7 @@
 <?php 
 use \Hcode\Page;
 use \Hcode\Model\Product;
+use \Hcode\Model\Category;
 
 $app->get('/', function(){
 
@@ -8,6 +9,17 @@ $app->get('/', function(){
 	$page = new Page();
 	$page->setTpl("index", [
 		'products'=>Product::checkList($products)
+	]);
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new Page();
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts())
 	]);
 });
 
